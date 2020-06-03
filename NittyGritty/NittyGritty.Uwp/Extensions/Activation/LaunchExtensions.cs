c1061b0e2dc7657c6755156baf4ab2ae6b785031
Mark Ivan Basto <16188252+MarkIvanDev@users.noreply.github.com>
2020-06-03 12:22:34 +0800

    add Launch Extensions

---
+++ NittyGritty/NittyGritty.Uwp/Extensions/Activation/LaunchExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+
+namespace NittyGritty.Uwp.Extensions.Activation
+{
+    public static class LaunchExtensions
+    {
+        public static bool IsPrimary(this LaunchActivatedEventArgs args)
+        {
+            return args.TileId == "App" && string.IsNullOrEmpty(args.Arguments);
+        }
+
+        public static bool IsSecondary(this LaunchActivatedEventArgs args)
+        {
+            return !string.IsNullOrEmpty(args.TileId) && args.TileId != "App";
+        }
+
+        public static bool IsJumplist(this LaunchActivatedEventArgs args)
+        {
+            return args.TileId == "App" && !string.IsNullOrEmpty(args.Arguments);
+        }
+
+        public static bool IsChaseable(this LaunchActivatedEventArgs args)
+        {
+            return !(args.TileActivatedInfo is null) && args.TileActivatedInfo.RecentlyShownNotifications.Count > 0;
+        }
+
+        public static LaunchSource GetLaunchSource(this LaunchActivatedEventArgs args)
+        {
+            if (args.IsPrimary())
+            {
+                return LaunchSource.Primary;
+            }
+            else if (args.IsSecondary())
+            {
+                return LaunchSource.Secondary;
+            }
+            else if (args.IsJumplist())
+            {
+                return LaunchSource.Jumplist;
+            }
+            else if (args.IsChaseable())
+            {
+                return LaunchSource.Chaseable;
+            }
+            else
+            {
+                return LaunchSource.Primary;
+            }
+        }
+    }
+
+    public enum LaunchSource
+    {
+        Primary = 0,
+        Secondary = 1,
+        Jumplist = 2,
+        Chaseable = 3
+    }
+}
