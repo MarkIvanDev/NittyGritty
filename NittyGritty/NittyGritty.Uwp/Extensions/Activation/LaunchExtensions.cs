a60c4ea357965dfcc9589ee6dc87f6635d6f7191
Mark Ivan Basto <16188252+MarkIvanDev@users.noreply.github.com>
2020-06-03 20:59:50 +0800

    add CommandLineExtensions and process method to LaunchActivation

--- NittyGritty/NittyGritty.Uwp/Extensions/Activation/LaunchExtensions.cs
+++ NittyGritty/NittyGritty.Uwp/Extensions/Activation/LaunchExtensions.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using NittyGritty.Models;
+using NittyGritty.Uwp.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml.Controls;
 
 namespace NittyGritty.Uwp.Extensions.Activation
 {
@@ -52,6 +55,39 @@ namespace NittyGritty.Uwp.Extensions.Activation
                 return LaunchSource.Primary;
             }
         }
+
+        public static void Process(this LaunchActivatedEventArgs args, LaunchActivationOptions options)
+        {
+            var source = args.GetLaunchSource();
+            switch (source)
+            {
+                case LaunchSource.Primary:
+                default:
+                    options.Frame.Navigate(options.PrimaryView);
+                    break;
+                case LaunchSource.Secondary:
+                    options.Frame.Navigate(
+                        options.ArgumentService.GetValue(
+                            args.Arguments, options.ActionKey)
+                            ?? options.PrimaryView,
+                        args.Arguments);
+                    break;
+                case LaunchSource.Jumplist:
+                    options.Frame.Navigate(
+                        options.ArgumentService.GetValue(
+                            args.Arguments, options.ActionKey)
+                            ?? options.PrimaryView,
+                        args.Arguments);
+                    break;
+                case LaunchSource.Chaseable:
+                    options.Frame.Navigate(
+                        options.ArgumentService.GetValue(
+                            args.TileActivatedInfo.RecentlyShownNotifications[0].Arguments, options.ActionKey)
+                            ?? options.PrimaryView,
+                        args.Arguments);
+                    break;
+            }
+        }
     }
 
     public enum LaunchSource
@@ -60,5 +96,16 @@ namespace NittyGritty.Uwp.Extensions.Activation
         Secondary = 1,
         Jumplist = 2,
         Chaseable = 3
+    }
+
+    public class LaunchActivationOptions
+    {
+        public Frame Frame { get; set; }
+
+        public Type PrimaryView { get; set; }
+
+        public string ActionKey { get; set; }
+
+        public ArgumentService ArgumentService { get; set; }
     }
 }
