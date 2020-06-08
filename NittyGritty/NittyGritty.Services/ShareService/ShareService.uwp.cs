c5a2042a661b91cd5b4ccd3f16793b66e01f6e2c
Mark Ivan Basto <16188252+MarkIvanDev@users.noreply.github.com>
2020-06-08 13:00:27 +0800

    use given DataPackage in args and set content there

--- NittyGritty/NittyGritty.Services/ShareService/ShareService.uwp.cs
+++ NittyGritty/NittyGritty.Services/ShareService/ShareService.uwp.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 using NittyGritty.Platform.Data;
 using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage;
+using Windows.Storage.Streams;
 
 namespace NittyGritty.Services
 {
@@ -36,21 +40,20 @@ namespace NittyGritty.Services
         {
             if (data != null)
             {
-                args.Request.Data = data.AsDataPackage();
-                //args.Request.Data.Properties.Title = data.Title;
-                //args.Request.Data.Properties.Description = data.Description;
+                args.Request.Data.Properties.Title = data.Title;
+                args.Request.Data.Properties.Description = data.Description;
 
-                //if (data.Text != null) args.Request.Data.SetText(data.Text);
-                //if (data.Html != null) args.Request.Data.SetHtmlFormat(data.Html);
-                //if (data.Rtf != null) args.Request.Data.SetRtf(data.Rtf);
-                //if (data.AppLink != null) args.Request.Data.SetApplicationLink(data.AppLink);
-                //if (data.WebLink != null) args.Request.Data.SetWebLink(data.WebLink);
-                //if (data.Bitmap != null) args.Request.Data.SetBitmap(RandomAccessStreamReference.CreateFromStream(data.Bitmap.AsRandomAccessStream()));
-                //if (data.StorageItems != null) args.Request.Data.SetStorageItems(data.StorageItems.Select(f => f.Context as IStorageItem));
-                //foreach (var data in data.CustomData)
-                //{
-                //    args.Request.Data.SetData(data.Key, data.Value);
-                //}
+                if (data.Text != null) args.Request.Data.SetText(data.Text);
+                if (data.Html != null) args.Request.Data.SetHtmlFormat(data.Html);
+                if (data.Rtf != null) args.Request.Data.SetRtf(data.Rtf);
+                if (data.AppLink != null) args.Request.Data.SetApplicationLink(data.AppLink);
+                if (data.WebLink != null) args.Request.Data.SetWebLink(data.WebLink);
+                if (data.Bitmap != null) args.Request.Data.SetBitmap(RandomAccessStreamReference.CreateFromStream(data.Bitmap.AsRandomAccessStream()));
+                if (data.StorageItems != null) args.Request.Data.SetStorageItems(data.StorageItems.Select(f => f.Context as IStorageItem));
+                foreach (var data in data.CustomData)
+                {
+                    args.Request.Data.SetData(data.Key, data.Value);
+                }
             }
         }
     }
