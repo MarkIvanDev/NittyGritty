d4c0e4c31b4718975acec53748544b713f4c995d
Mark Ivan Basto <markivan.developer@outlook.com>
2020-07-17 23:11:52 +0800

    add ILauncherService

---
+++ NittyGritty/NittyGritty.Platform/Launcher/LaunchType.shared.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NittyGritty.Platform.Launcher
+{
+    public enum LaunchType
+    {
+        Uri = 0,
+        UriForResults = 1
+    }
+}
