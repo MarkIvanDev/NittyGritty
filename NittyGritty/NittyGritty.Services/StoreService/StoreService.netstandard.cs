5d09c1091f3816a8c2d9506c46c4ada45bf14f2d
Mark Ivan Basto <markivan.developer@outlook.com>
2020-08-12 11:17:31 +0800

    implement StoreService

--- NittyGritty/NittyGritty.Services/StoreService/StoreService.netstandard.cs
+++ NittyGritty/NittyGritty.Services/StoreService/StoreService.netstandard.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace NittyGritty.Services
 {
     public partial class StoreService
     {
+
+        Task<bool> PlatformCheckForUpdates()
+            => throw new NotImplementedException();
+
+        Task<bool> PlatformCheckForMandatoryUpdates()
+            => throw new NotImplementedException();
+
+        Task PlatformDownloadUpdates()
+            => throw new NotImplementedException();
+
+        Task PlatformDownloadAndInstallUpdates()
+            => throw new NotImplementedException();
+
     }
 }
