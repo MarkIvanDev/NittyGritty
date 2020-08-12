333a33f58f75bbae5f5d2f9fc39d86d4c114f3b2
Mark Ivan Basto <markivan.developer@outlook.com>
2020-08-12 15:56:24 +0800

    add ByteArrayExtensions

---
+++ NittyGritty/NittyGritty/Extensions/ByteArrayExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NittyGritty.Extensions
+{
+    public static class ByteArrayExtensions
+    {
+        public static MemoryStream ToMemoryStream(this byte[] byteArray)
+        {
+            if (byteArray is null)
+            {
+                return null;
+            }
+
+            var memoryStream = new MemoryStream();
+            memoryStream.Write(byteArray, 0, byteArray.Length);
+            return memoryStream;
+        }
+    }
+}
