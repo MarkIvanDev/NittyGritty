6fae6dad80fc38f27dcc996d73cd164c8324d29d
Mark Ivan Basto <markivan.developer@outlook.com>
2020-10-14 19:38:26 +0800

    add RightTappedSelectionBehavior and ScrollSelectedItemIntoViewBehavior

--- NittyGritty/NittyGritty.Uwp/Extensions/DependencyObjectExtensions.cs
+++ NittyGritty/NittyGritty.Uwp/Extensions/DependencyObjectExtensions.cs
@@ -26,39 +26,5 @@ namespace NittyGritty.Uwp.Extensions
         public static readonly DependencyProperty TagProperty =
             DependencyProperty.RegisterAttached("Tag", typeof(object), typeof(DependencyObjectExtensions), new PropertyMetadata(null));
 
-        public static IList<T> FindChildren<T>(this DependencyObject startNode) where T : DependencyObject
-        {
-            var results = new List<T>();
-            int count = VisualTreeHelper.GetChildrenCount(startNode);
-            for (int i = 0; i < count; i++)
-            {
-                DependencyObject current = VisualTreeHelper.GetChild(startNode, i);
-                if ((current.GetType()).Equals(typeof(T)) || (current.GetType().GetTypeInfo().IsSubclassOf(typeof(T))))
-                {
-                    T asType = (T)current;
-                    results.Add(asType);
-                }
-                var children = FindChildren<T>(current);
-                results.AddRange(children);
-            }
-            return results;
-        }
-
-        public static T FindParent<T>(this DependencyObject child) where T : DependencyObject
-        {
-            T parent = null;
-            DependencyObject CurrentParent = VisualTreeHelper.GetParent(child);
-            while (CurrentParent != null)
-            {
-                if (CurrentParent is T)
-                {
-                    parent = (T)CurrentParent;
-                    break;
-                }
-                CurrentParent = VisualTreeHelper.GetParent(CurrentParent);
-
-            }
-            return parent;
-        }
     }
 }
