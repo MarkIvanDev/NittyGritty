cf2bed933258e97ef9911faf717c52adb395f27b
Mark Ivan Basto <markivan.developer@outlook.com>
2020-11-11 16:21:08 +0800

    add DownloadService

---
+++ NittyGritty/NittyGritty.Platform/Download/DownloadProgressChangedEventArgs.shared.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NittyGritty.Platform.Download
+{
+    public class DownloadProgressChangedEventArgs
+    {
+        public DownloadProgressChangedEventArgs(
+            string downloadPath, DownloadStatus status = DownloadStatus.Idle, double? total = null, double downloaded = 0)
+        {
+            DownloadPath = downloadPath;
+            Status = status;
+            Total = total;
+            Downloaded = downloaded;
+        }
+
+        public string DownloadPath { get; }
+
+        public DownloadStatus Status { get; }
+
+        public double? Total { get; }
+
+        public double Downloaded { get; }
+
+        public double Percentage => Total.HasValue ? Downloaded / Total.Value : 0;
+
+        public string ErrorMessage { get; set; }
+    }
+
+    public delegate void DownloadProgressChangedEventHandler(object sender, DownloadProgressChangedEventArgs args);
+}
---
+++ NittyGritty/NittyGritty.Services/DownloadService/DownloadService.shared.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NittyGritty.Platform.Download;
+using NittyGritty.Platform.Storage;
+
+namespace NittyGritty.Services
+{
+    public partial class DownloadService : IDownloadService
+    {
+        public event DownloadProgressChangedEventHandler DownloadProgressChanged;
+
+        public Task Initialize()
+            => PlatformInitialize();
+
+        public Task Download(Uri uri)
+            => PlatformDownload(uri);
+
+        public Task Download(Uri uri, IFile file)
+            => PlatformDownload(uri, file);
+
+        public void Pause(Uri uri)
+            => PlatformPause(uri);
+
+        public void Resume(Uri uri)
+            => PlatformResume(uri);
+
+        public void Cancel(Uri uri)
+            => PlatformCancel(uri);
+    }
+}
