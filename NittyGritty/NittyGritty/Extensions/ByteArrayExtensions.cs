10fec32b957a63f6586f1fbf5f5ac110e1be89a7
Mark Ivan Basto <markivan.developer@outlook.com>
2021-03-12 21:22:50 +0800

    add ToByteArray extension method for Stream

--- NittyGritty/NittyGritty/Extensions/ByteArrayExtensions.cs
+++ NittyGritty/NittyGritty/Extensions/ByteArrayExtensions.cs
@@ -18,5 +18,19 @@ namespace NittyGritty.Extensions
             memoryStream.Write(byteArray, 0, byteArray.Length);
             return memoryStream;
         }
+
+        public static byte[] ToByteArray(this Stream stream)
+        {
+            if (stream != null)
+            {
+                using (var memory = new MemoryStream())
+                {
+                    stream.Position = 0;
+                    stream.CopyTo(memory);
+                    return memory.ToArray();
+                }
+            }
+            return null;
+        }
     }
 }
