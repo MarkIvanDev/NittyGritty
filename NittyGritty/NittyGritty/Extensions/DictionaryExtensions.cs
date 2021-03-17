f8cb7a4b7a23eb9dd9d7a7cf0305fbeb6a137dca
Mark Ivan Basto <markivan.developer@outlook.com>
2021-03-17 18:14:05 +0800

    add Dictionary extensions

---
+++ NittyGritty/NittyGritty/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NittyGritty.Extensions
+{
+    public static class DictionaryExtensions
+    {
+
+        public static TValue Get<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue = default)
+        {
+            return dictionary.TryGetValue(key, out var value) ?
+                value : defaultValue;
+        }
+
+        public static TValue Extract<TKey, TValue>(this IDictionary<TKey, object> dictionary, TKey key, TValue defaultValue = default)
+        {
+            return dictionary.TryGetValue(key, out var v) && v is TValue value ?
+                value : defaultValue;
+        }
+
+        public static TValue Extract<TValue>(this IDictionary<string, object> dictionary, string key, TValue defaultValue = default)
+        {
+            return Extract<string, TValue>(dictionary, key, defaultValue);
+        }
+
+    }
+}
