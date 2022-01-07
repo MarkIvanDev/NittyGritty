277cada1c3b838c849ee4024bfffe877f7dc8a6c
Mark Ivan Basto <markivan.developer@outlook.com>
2022-01-07 15:16:27 +0800

    add uwp converters

---
+++ NittyGritty/NittyGritty.Uwp/Converters/BooleanValueConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace NittyGritty.Uwp.Converters
+{
+    public class BooleanValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (bool.TryParse(value?.ToString(), out var result))
+            {
+                return result ? TrueValue : FalseValue;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object TrueValue { get; set; }
+
+        public object FalseValue { get; set; }
+
+    }
+}
---
+++ NittyGritty/NittyGritty.Uwp/Converters/DrawingColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+
+namespace NittyGritty.Uwp.Converters
+{
+    public class DrawingColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is System.Drawing.Color colorInfo)
+            {
+                return Color.FromArgb(colorInfo.A, colorInfo.R, colorInfo.G, colorInfo.B);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Color color)
+            {
+                return System.Drawing.Color.FromArgb(color.A, color.R, color.G, color.B);
+            }
+            return null;
+        }
+    }
+}
