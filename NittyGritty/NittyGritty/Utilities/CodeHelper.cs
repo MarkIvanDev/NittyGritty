a507a8e4c7452af4f72bf679e20e9a83ed38ccde
Mark Ivan Basto <markivan.developer@outlook.com>
2022-02-28 18:01:10 +0800

    add CodeHelper for try catch

---
+++ NittyGritty/NittyGritty/Utilities/CodeHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NittyGritty.Utilities
+{
+    public static class CodeHelper
+    {
+
+        public static T InvokeOrDefault<T>(Func<T> func, T defaultValue = default)
+        {
+            try
+            {
+                return func();
+            }
+            catch (Exception)
+            {
+                return defaultValue;
+            }
+        }
+
+        public static async Task<T> InvokeOrDefault<T>(Func<Task<T>> func, T defaultValue = default)
+        {
+            try
+            {
+                return await func();
+            }
+            catch (Exception)
+            {
+                return defaultValue;
+            }
+        }
+
+    }
+}
