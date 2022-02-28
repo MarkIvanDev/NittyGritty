72ef60a9bd170cd2158c99855dd8d234ce182403
Mark Ivan Basto <markivan.developer@outlook.com>
2022-02-28 20:38:11 +0800

    add SwitchConverter

---
+++ NittyGritty/NittyGritty.Uno/Converters/SwitchConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NittyGritty.Extensions;
+using Windows.UI.Xaml.Data;
+
+namespace NittyGritty.Uno.Converters
+{
+    public class SwitchConverter : Collection<Case>, IValueConverter
+    {
+        public Case Default { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var pick = this.FirstOrDefault(c => value == c.Value || (value?.EqualTo(c.Value) ?? false) || (c.Value?.EqualTo(value) ?? false));
+            return pick?.Result ?? Default?.Result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Case
+    {
+        public object Value { get; set; }
+
+        public object Result { get; set; }
+    }
+}
