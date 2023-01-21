92e40a3d6f6c121f2570850933fa939874711bf6
Mark Ivan Basto <markivan.developer@outlook.com>
2023-01-21 22:38:19 +0800

    add theme support to dialog service

--- NittyGritty/NittyGritty.Uno.Services/DialogService.cs
+++ NittyGritty/NittyGritty.Uno.Services/DialogService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NittyGritty.Platform.Theme;
 using NittyGritty.Services.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -30,20 +31,54 @@ namespace NittyGritty.Uno.Services
         }
 
         public async Task<bool> Show(string key)
-        {
-            return await Show<object>(key, null, null);
-        }
+            => await Show(key, AppTheme.Default);
+
+        public async Task<bool> Show(string key, AppTheme theme)
+            => await Show<object>(key, null, null, theme);
 
         public async Task<bool> Show<T>(string key, T parameter)
+            => await Show(key, parameter, AppTheme.Default);
+
+        public async Task<bool> Show<T>(string key, T parameter, AppTheme theme)
+            => await Show(key, parameter, null, theme);
+
+        public async Task ShowMessage(string message, string title)
+            => await ShowMessage(message, title, AppTheme.Default);
+
+        public async Task ShowMessage(string message, string title, AppTheme theme)
+            => await ShowMessage(message, title, null, theme);
+
+        public async Task ShowMessage(string message, string title, string buttonText)
+            => await ShowMessage(message, title, buttonText, AppTheme.Default);
+
+        public async Task ShowMessage(string message, string title, string buttonText, AppTheme theme)
+            => await CreateDialog(message, title, buttonText, theme: theme).ShowAsync();
+
+        public async Task<bool> ShowMessage(string message, string title, string buttonConfirmText, string buttonCancelText)
+            => await ShowMessage(message, title, buttonConfirmText, buttonCancelText, AppTheme.Default);
+
+        public async Task<bool> ShowMessage(string message, string title, string buttonConfirmText, string buttonCancelText, AppTheme theme)
         {
-            return await Show(key, parameter, null);
+            var dialog = CreateDialog(
+                message,
+                title,
+                buttonConfirmText,
+                buttonCancelText,
+                theme);
+
+            var result = await dialog.ShowAsync();
+            return result == ContentDialogResult.Primary;
         }
 
         public async Task<bool> Show<T>(string key, T parameter, Func<T, Task> onOpened)
+            => await Show(key, parameter, onOpened, AppTheme.Default);
+
+        public async Task<bool> Show<T>(string key, T parameter, Func<T, Task> onOpened, AppTheme theme)
         {
             var instance = Activator.CreateInstance(GetValue(key));
             if (instance is ContentDialog dialog)
             {
+                dialog.RequestedTheme = (ElementTheme)theme;
                 if (onOpened != null)
                 {
                     dialog.Opened += async (sender, e) =>
@@ -61,41 +96,20 @@ namespace NittyGritty.Uno.Services
             return false;
         }
 
-        public async Task ShowMessage(string message, string title)
-        {
-            await ShowMessage(message, title, null);
-        }
-
-        public async Task ShowMessage(string message, string title, string buttonText)
-        {
-            var dialog = CreateDialog(message, title, buttonText);
-            await dialog.ShowAsync();
-        }
-
-        public async Task<bool> ShowMessage(string message, string title, string buttonConfirmText, string buttonCancelText)
-        {
-            var dialog = CreateDialog(
-                message,
-                title,
-                buttonConfirmText,
-                buttonCancelText);
-
-            var result = await dialog.ShowAsync();
-            return result == ContentDialogResult.Primary;
-        }
-
         private ContentDialog CreateDialog(
             string message,
             string title,
             string buttonConfirmText = null,
-            string buttonCancelText = null)
+            string buttonCancelText = null,
+            AppTheme theme = AppTheme.Default)
         {
             var dialog = new ContentDialog()
             {
                 Title = title,
                 Content = message,
                 PrimaryButtonText = !string.IsNullOrEmpty(buttonConfirmText) ? buttonConfirmText : "OK",
-                DefaultButton = ContentDialogButton.Primary
+                DefaultButton = ContentDialogButton.Primary,
+                RequestedTheme = (ElementTheme)theme,
             };
 
             if (!string.IsNullOrEmpty(buttonCancelText))
--- NittyGritty/NittyGritty.Uwp.Services/DialogService.cs
+++ NittyGritty/NittyGritty.Uwp.Services/DialogService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NittyGritty.Platform.Theme;
 using NittyGritty.Services.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -29,45 +30,55 @@ namespace NittyGritty.Uwp.Services
             }
         }
 
+        public async Task<bool> Show(string key) 
+            => await Show(key, AppTheme.Default);
 
-        public async Task<bool> Show(string key)
-        {
-            return await Show<object>(key, null, null);
-        }
+        public async Task<bool> Show(string key, AppTheme theme) 
+            => await Show<object>(key, null, null, theme);
 
-        public async Task<bool> Show<T>(string key, T parameter)
-        {
-            return await Show(key, parameter, null);
-        }
+        public async Task<bool> Show<T>(string key, T parameter) 
+            => await Show(key, parameter, AppTheme.Default);
 
-        public async Task ShowMessage(string message, string title)
-        {
-            await ShowMessage(message, title, null);
-        }
+        public async Task<bool> Show<T>(string key, T parameter, AppTheme theme) 
+            => await Show(key, parameter, null, theme);
 
-        public async Task ShowMessage(string message, string title, string buttonText)
-        {
-            var dialog = CreateDialog(message, title, buttonText);
-            await dialog.ShowAsync();
-        }
+        public async Task ShowMessage(string message, string title) 
+            => await ShowMessage(message, title, AppTheme.Default);
 
-        public async Task<bool> ShowMessage(string message, string title, string buttonConfirmText, string buttonCancelText)
+        public async Task ShowMessage(string message, string title, AppTheme theme) 
+            => await ShowMessage(message, title, null, theme);
+
+        public async Task ShowMessage(string message, string title, string buttonText) 
+            => await ShowMessage(message, title, buttonText, AppTheme.Default);
+
+        public async Task ShowMessage(string message, string title, string buttonText, AppTheme theme) 
+            => await CreateDialog(message, title, buttonText, theme: theme).ShowAsync();
+
+        public async Task<bool> ShowMessage(string message, string title, string buttonConfirmText, string buttonCancelText) 
+            => await ShowMessage(message, title, buttonConfirmText, buttonCancelText, AppTheme.Default);
+
+        public async Task<bool> ShowMessage(string message, string title, string buttonConfirmText, string buttonCancelText, AppTheme theme)
         {
             var dialog = CreateDialog(
                 message,
                 title,
                 buttonConfirmText,
-                buttonCancelText);
+                buttonCancelText,
+                theme);
 
             var result = await dialog.ShowAsync();
             return result == ContentDialogResult.Primary;
         }
 
-        public async Task<bool> Show<T>(string key, T parameter, Func<T, Task> onOpened)
+        public async Task<bool> Show<T>(string key, T parameter, Func<T, Task> onOpened) 
+            => await Show(key, parameter, onOpened, AppTheme.Default);
+
+        public async Task<bool> Show<T>(string key, T parameter, Func<T, Task> onOpened, AppTheme theme)
         {
             var instance = Activator.CreateInstance(GetValue(key));
             if (instance is ContentDialog dialog)
             {
+                dialog.RequestedTheme = (ElementTheme)theme;
                 if (onOpened != null)
                 {
                     dialog.Opened += async (sender, e) =>
@@ -89,14 +100,16 @@ namespace NittyGritty.Uwp.Services
             string message,
             string title,
             string buttonConfirmText = null,
-            string buttonCancelText = null)
+            string buttonCancelText = null,
+            AppTheme theme = AppTheme.Default)
         {
             var dialog = new ContentDialog()
             {
                 Title = title,
                 Content = message,
                 PrimaryButtonText = !string.IsNullOrEmpty(buttonConfirmText) ? buttonConfirmText : "OK",
-                DefaultButton = ContentDialogButton.Primary
+                DefaultButton = ContentDialogButton.Primary,
+                RequestedTheme = (ElementTheme)theme,
             };
 
             if (!string.IsNullOrEmpty(buttonCancelText))
